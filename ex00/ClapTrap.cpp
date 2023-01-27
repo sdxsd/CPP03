c3ed5d99c6fc4424a3f6f221b9bc035a6560a277
sdxsd <willmaguire@icloud.com>
2023-01-27 19:12:13 +0100

    Gezoept.

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,6 +1,7 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(std::string title) {
+	std::cout << "ClapTrap constructor called." << std::endl;
 	name = title;
 	HP = 10;
 	EP = 10;
@@ -8,5 +9,21 @@ ClapTrap::ClapTrap(std::string title) {
 }
 
 ClapTrap::ClapTrap(const ClapTrap& clap) {
+	std::cout << "ClapTrap copy constructor called." << std::endl;
+	name = clap.name;
+	HP = clap.HP;
+	EP = clap.EP;
+	DMG = clap.DMG;
+}
 
+ClapTrap::~ClapTrap(void) {
+	std::cout << "ClapTrap deconstructor called." << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &clap) {
+	this->name = clap.name;
+	this->HP = clap.HP;
+	this->EP = clap.EP;
+	this->DMG = clap.DMG;
+	return (*this);
 }
