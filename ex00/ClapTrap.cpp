3d715d567bb3240c39007e3be2c13df58bcd0993
sdxsd <willmaguire@icloud.com>
2023-01-27 20:01:48 +0100

    Too much useless typing.

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -27,3 +27,25 @@ ClapTrap& ClapTrap::operator=(const ClapTrap &clap) {
 	this->DMG = clap.DMG;
 	return (*this);
 }
+
+void ClapTrap::attack(const std::string& target) {
+	if (EP--)
+		std::cout << "Claptrap " << name << "Attacks" << target
+				  << ", causing " << DMG << "points of damage!";
+	else
+		std::cout << "ClapTrap " << name << "out of energy." << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	std::cout << "ClapTrap " << name << "took" << amount << "damage" << std::endl;
+	HP -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int value) {
+	if (EP--) {
+		std::cout << "ClapTrap " << name << "repaired by " << value << std::endl;
+		HP += value;
+	}
+	else
+		std::cout << "ClapTrap " << name << "too tired to repair" << std::endl;
+}
